c2293b4bee4b797164d63952db7608036529480b
matiasott <jorgematiasott@gmail.com>
2023-03-02 20:26:36 -0300

    home past y upcomig funcionando

--- script/home.js
+++ script/home.js
@@ -2,33 +2,47 @@ var data = data
 
 const fecha_actual= data.currentDate
 
+const arrayData = data.events
 
-let arrayData = data.events
+// for(i = 0;i < arrayData.length; i++){
+//     let id = arrayData[i]._id
+//     let image = arrayData[i].image
+//     let name = arrayData[i].name
+//     let date = arrayData[i].date
+//     let description = arrayData[i].description
+//     let category = arrayData[i].category
+//     let place = arrayData[i].place
+//     let capacity = arrayData[i].capacity
+//     let assistance = arrayData[i].assistance
+//     let price = arrayData[i].price
 
-for(i = 0;i < arrayData.length; i++){
-    let id = arrayData[i]._id
-    let image = arrayData[i].image
-    let name = arrayData[i].name
-    let date = arrayData[i].date
-    let description = arrayData[i].description
-    let category = arrayData[i].category
-    let place = arrayData[i].place
-    let capacity = arrayData[i].capacity
-    let assistance = arrayData[i].assistance
-    let price = arrayData[i].price
+//     console.log("_________________________________________")
 
-    console.log("_________________________________________")
+//     console.log(" ESTE ES EL ID:", id)
+//     console.log("image:", image)
+//     console.log("name:", name )
+//     console.log("date:", date )
+//     console.log("description:", description )
+//     console.log("category:", category )
+//     console.log("place:", place  )
+//     console.log("capacity :", capacity )
+//     console.log("assistance:", assistance )
+//     console.log("price:", price )
+// }
 
-    console.log(" ESTE ES EL ID:", id)
-    console.log("image:", image)
-    console.log("name:", name )
-    console.log("date:", date )
-    console.log("description:", description )
-    console.log("category:", category )
-    console.log("place:", place  )
-    console.log("capacity :", capacity )
-    console.log("assistance:", assistance )
-    console.log("price:", price )
-}
+let card = document.getElementById("cart-home")
+
+arrayData.map((x) => {
+    card.innerHTML += `<article class="article-home">
+    <div class="card" style="width: 18rem;">
+        <img src="${x.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${x.name}</h5>
+          <p class=${x.description}</p>
+          <a href="./car_details.html" class="btn btn-primary">See more</a>
+            <h6 class="precio">$ ${x.price}</h6>
+        </div>
+      </div>`
+})
 
 
--- script/past_events.js
+++ script/past_events.js
@@ -2,45 +2,56 @@ var data = data
 
 const fecha_actual= data.currentDate
 
-
 let arrayData = data.events
 
-let contador = 0
+// let contador = 0
 
-for(i = 0;i < arrayData.length; i++){
+// for(i = 0;i < arrayData.length; i++){
 
-    let date = arrayData[i].date
+//     let date = arrayData[i].date
 
-    if(fecha_actual > date && contador < 7){
+//     if(fecha_actual > date && contador < 7){
 
-        let id = arrayData[i]._id
-        let image = arrayData[i].image
-        let name = arrayData[i].name
-        
-        
-        let description = arrayData[i].description
-        let category = arrayData[i].category
-        let place = arrayData[i].place
-        let capacity = arrayData[i].capacity
-        let assistance = arrayData[i].assistance
-        let price = arrayData[i].price
+//         let id = arrayData[i]._id
+//         let image = arrayData[i].image
+//         let name = arrayData[i].name
+//         let description = arrayData[i].description
+//         let category = arrayData[i].category
+//         let place = arrayData[i].place
+//         let capacity = arrayData[i].capacity
+//         let assistance = arrayData[i].assistance
+//         let price = arrayData[i].price
 
-        console.log("_________________________________________")
+//         console.log("_________________________________________")
 
-        console.log(" ESTE ES EL ID:", id)
-        console.log("image:", image)
-        console.log("name:", name )
-        console.log("date:", date )
-        console.log("description:", description )
-        console.log("category:", category )
-        console.log("place:", place  )
-        console.log("capacity :", capacity )
-        console.log("assistance:", assistance )
-        console.log("price:", price )
+//         console.log(" ESTE ES EL ID:", id)
+//         console.log("image:", image)
+//         console.log("name:", name )
+//         console.log("date:", date )
+//         console.log("description:", description )
+//         console.log("category:", category )
+//         console.log("place:", place  )
+//         console.log("capacity :", capacity )
+//         console.log("assistance:", assistance )
+//         console.log("price:", price )
 
-        contador++
-    }
-}
+//         contador++
+//     }
+// }
 
 
+let card = document.getElementById("cart-past")
 
+arrayData.map((x) => {
+    if(fecha_actual>x.date){
+    card.innerHTML += `<article class="article-home">
+    <div class="card" style="width: 18rem;">
+        <img src="${x.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${x.name}</h5>
+          <p class=${x.description}</p>
+          <a href="./car_details.html" class="btn btn-primary">See more</a>
+            <h6 class="precio">$ ${x.price}</h6>
+        </div>
+      </div>`}
+})
