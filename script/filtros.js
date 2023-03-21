85142f61040538882374102ce3a7b03f66db2c49
matiasott <jorgematiasott@gmail.com>
2023-03-21 18:21:03 -0300

    actualizacion

--- script/filtros.js
+++ script/filtros.js
@@ -17,13 +17,13 @@
         <div class="card" style="width: 18rem;">
             <img src="${x.image}" class="card-img-top" alt="${x.name} event image">
             <div class="card-body">
-              <h5 class="card-title">${x.name}</h5>
-              <p class=${x.description}</p>
-              <a href="./car_details.html?id=${x._id}" class="btn btn-primary">See more</a>
+            <h5 class="card-title">${x.name}</h5>
+            <p class=${x.description}</p>
+            <a href="./car_details.html?id=${x._id}" class="btn btn-primary">See more</a>
                 <h6 class="precio">$ ${x.price}</h6>
             </div>
-          </div>
-          </article>`
+        </div>
+        </article>`
         }
         
         if (window.location.href.endsWith("past_events.html")) {
@@ -32,13 +32,13 @@
         <div class="card" style="width: 18rem;">
             <img src="${x.image}" class="card-img-top" alt="${x.name} event image">
             <div class="card-body">
-              <h5 class="card-title">${x.name}</h5>
-              <p class=${x.description}</p>
-              <a href="./car_details.html?id=${x._id}" class="btn btn-primary">See more</a>
+            <h5 class="card-title">${x.name}</h5>
+            <p class=${x.description}</p>
+            <a href="./car_details.html?id=${x._id}" class="btn btn-primary">See more</a>
                 <h6 class="precio">$ ${x.price}</h6>
             </div>
-          </div>
-          </article>`
+        </div>
+        </article>`
         }
     }
         
